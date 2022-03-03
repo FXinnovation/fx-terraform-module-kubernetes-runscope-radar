ebe40101487c41d980f23e49154fa8161de488bd
david <david.glantenay@fxinnovation.com>
2022-03-03 17:28:48 -0500

    fix: replace obsolete list function by a litteral list

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "deployment" {
-  value = element(concat(kubernetes_deployment.this.*, list({})), 0)
+  value = element(concat(kubernetes_deployment.this.*, []), 0)
 }
 
 output "secret" {
-  value     = element(concat(kubernetes_secret.this.*, list({})), 0)
+  value     = element(concat(kubernetes_secret.this.*, []), 0)
   sensitive = true
 }
